5027efa63e53bfb561869e4d57bac9b4ebcc80a1
Tianxiaolong <99946153+na5nami@users.noreply.github.com>
2022-04-07 16:02:10 +0000

    Create
    structure
    
    data

--- src/main.c
+++ src/main.c
@@ -1,20 +1,17 @@
-/* hello.c */
 #include <stdio.h>
-
 struct StudentData{
 char *stu_name;
 int stu_id;
 int stu_age;
 };
-int main ()
-{
+int main(){
   struct StudentData student;
-  student.stu_name = "Maryia";
-  student.stu_id = 123456;
-  student.stu_age = 45;
-  
-	printf ("Hello,%s",student.stu_name);
-	printf ("\nStudent Id is: %d",student.stu_id);
-  printf("\nStudent Agebis: %d",student.stu_age);
+  student.stu_name="tianxiaolong";
+  student.stu_id=2023464;
+  student.stu_age=22;
+  printf("Hello, %s",student.stu_name);
+  printf("\nStudent Id is: %d",student.stu_id);
+  printf("\nStudent Age is: %d",student.stu_age);
+  printf("\nGoodbye");
   return 0;
 }
